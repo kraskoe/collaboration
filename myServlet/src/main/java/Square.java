fe12b7b3b40c21a01db4c3cde76182f3b08b08f2
kraskoe <krasko_e@mail.ru>
2017-09-27 21:47:32 +0300

    Revert "Circle"

--- myServlet/src/main/java/Square.java
+++ myServlet/src/main/java/Square.java
@@ -1,8 +1,5 @@
 public class Square {
     public double square(double a, double b){
-        System.out.println("This is square");
-        System.out.println("SOme troubles...");
-        System.out.println("Hi");
         return a*b;
     }
 }
