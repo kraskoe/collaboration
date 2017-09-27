8a8bc4c2a0cbdf032128052dc6dae1c90509eaec
kraskoe <krasko_e@mail.ru>
2017-09-27 21:19:07 +0300

    square sout added

--- myServlet/src/main/java/Square.java
+++ myServlet/src/main/java/Square.java
@@ -1,5 +1,7 @@
 public class Square {
     public double square(double a, double b){
+        System.out.println("This is square");
+        System.out.println("SOme troubles...");
         return a*b;
     }
 }
